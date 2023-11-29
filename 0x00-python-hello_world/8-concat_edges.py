723f385b9dbe85164a32b29e2cdd18621f702f7b
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 08:09:17 -0800

    concatentate with slices

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+#Extracting&printing desired subscripting using slicing and concatetation
+print(str[38:66],"with python",end='\n')
