7fbbd938c130b565af7015fcbc350a1289303549
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 01:44:48 -0800

    guru

--- 0x00-python-hello_world/2-print.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-message = '"Programming is like building a multilingual puzzle'
-#print function  used to display the message
-print(message)
-#Newline after the message
-print('\n')
