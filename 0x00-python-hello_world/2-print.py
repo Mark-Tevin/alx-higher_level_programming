fd1679c7c6236ef693c1926888424012a0825f26
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 01:06:53 -0800

    hard

--- 0x00-python-hello_world/2-print.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-#python script that prints a specific message and leaves a line
-message = "Programming is like building a multilingual puzzle"
-#print function to display the message followed by anew line
-print(message, end='\n')
