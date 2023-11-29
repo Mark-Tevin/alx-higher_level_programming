ee952f0bd5a32636e2ff45a643cccc995d4ca541
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 04:55:04 -0800

    question 3 corrected

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 number = 98
-#consider the number stored in varaible called number, followed by the rest of statments
-print(f"{number} Battery street", end='\n')
-
+print(f"{number} Battery street", end='\n')#consider the number stored in varaible called number, followed by the rest of statments
