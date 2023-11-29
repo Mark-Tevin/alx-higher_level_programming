122bf0cb7be4f49ed3306a1bc9eddde2481d4d80
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 04:17:24 -0800

    print string

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Holberton School"
+#printing the string three times then followed by a new line
+print(f"{str}" * 3,end='\n')
+print(str[:9], end='\n')#first nine characters of the string followed by a new line
