799f87de927c4f2a7c4ee6db208e047ce76006e1
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 01:22:38 -0800

    print message followed by anew line

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-#print function
-message = "Programming is like building a multilingual puzzle"
-
-print(message, end='\n')
+#use of print function to print message followed by a new line
+print("programming is like building a multilingual puzzle", end="\n")
