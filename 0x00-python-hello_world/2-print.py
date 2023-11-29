69ecf20704b2a4bb6f4f137c915433ca9e82a3ae
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 03:58:45 -0800

    2-print.py

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-print('"Programming is like building a multilingual puzzle')
-print("\n")  # This adds a new line
+print('"Programming is like building a multilingual puzzle',end='\n')
