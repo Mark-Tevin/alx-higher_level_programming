70345107aae7ac9c31eac973274745d34dcd408f
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 01:43:23 -0800

    print function to show specific message followed by anew line

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 message = '"Programming is like building a multilingual puzzle'
-
-#print function
-print(message, end='\n')
+#print function  used to display the message
+print(message)
+#Newline after the message
+print('\n')
