727c539382ac902a2372257aed1256e0a7809e5e
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 17:36:08 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-# Program to print ASCII alphabet in lowercase (excluding 'q' and 'e') without a new line
-print("".join("{}".format(chr(char)) for char in range(ord('a'), ord('z') + 1) if chr(char) not in ('q', 'e')), end='')
+# Print ASCII alphabet in lowercase excluding 'q' and 'e' without a new line
+for char in range(ord('a'), ord('z') + 1):
+    if chr(char) not in ['q', 'e']:
+        print("{}".format(chr(char)), end="")
