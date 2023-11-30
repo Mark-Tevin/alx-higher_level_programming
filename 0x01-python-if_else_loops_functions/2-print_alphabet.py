9b1df8e22204db05d896a71acd790e4c824be329
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 16:45:21 -0800

    correction

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # Program to print ASCII alphabet in lowercase without a new line
 for char in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr(char)), end="")
+    print("{}".format(chr(char)), end=" ")
 
