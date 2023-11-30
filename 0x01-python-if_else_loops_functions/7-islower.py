a6503563d971b02c807a7896059753d17636a325
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 18:29:56 -0800

    7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,10 +1,7 @@
-#!/usr/bin/python
-
 def islower(c):
-    # Check if the ASCII value of the character is in the lowercase range
     return ord('a') <= ord(c) <= ord('z')
 
-# Example usage
-print(islower('a'))  # True
-print(islower('A'))  # False
-print(islower('1'))  # False
+# Example usage:
+char = 'a'
+print(islower(char))  # Output: True
+
