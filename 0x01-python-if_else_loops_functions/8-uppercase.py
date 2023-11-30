b9ced357f1f598f8d0d83dc5a2131acc344b1f1e
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 18:52:33 -0800

    8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def uppercase(t):
-    output = ""
+    case = ""
     for char in t:
         if 'a' <= char <= 'z':
-            output += "{}".format(chr(ord(char) - 32))
+            case += "{}".format(chr(ord(char) - 32))
         else:
-            output += char
-    print(output)
+            case += char
+    print(case)
