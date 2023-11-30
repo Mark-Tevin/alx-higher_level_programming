e63aab9191cbf13f84733efdf20ba5e456fd8fb2
Mark-Tevin <tevinsmark@gmail.com>
2023-11-29 17:09:01 -0800

    python loops

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 # Program to print ASCII alphabet in lowercase without a new line
 for i in range(97, 123):
-    print("{}".format(chr(1), end=')
+    print("{}".format(chr(1), end='')
