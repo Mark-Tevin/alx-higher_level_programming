84c13df3af95f3c8fb3c6f1a52cafc2ee74c7105
Mark-Tevin <tevinsmark@gmail.com>
2023-12-03 10:39:12 -0800

    corrected importfile

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,11 @@
-#consists of the def add(a,b)
-a = 1
-b = 2
+#!/usr/bin/python3
 
-from add_0 import add
-s = add(a, b)
-print(f"{a} + {b} = {s}")
+if __name__ == "__main__":
+    a = 1
+    b = 2
+
+    from add_0 import add
+
+    output = add(a, b)
+    print("{} + {} = {}".format(a, b, output))
+
