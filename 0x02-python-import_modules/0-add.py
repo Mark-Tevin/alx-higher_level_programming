c036d7eb4c0ad57d24ffd575b5a5f9bee735dc39
Mark-Tevin <tevinsmark@gmail.com>
2023-12-06 02:23:02 -0800

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,9 +3,10 @@ if __name__ == "__main__":
 
     import sys
 
-from add_0 import add
-a = 1
-b = 2
-output = add(a,b)
+    from add_0 import add
+    a = 1
+    b = 2
 
-print("{} + {} = {}".format(a, b, output))
+    output = add(a,b)
+
+    print("{} + {} = {}".format(a, b, output))
