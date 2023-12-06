ff4b1f4b48dc03d3463d405f2d6a514cfdfedd7a
Mark-Tevin <tevinsmark@gmail.com>
2023-12-06 12:14:09 -0800

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,8 @@
-5-variable_load.py
 #!/usr/bin/python3
+
 from variable_load_5 import a
-print(f"The value of variable 'a' is: {a}")
+def find_var():
+    print(a)
+
+if __name__ == "__main__":
+    find_var()
