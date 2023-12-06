8e05b43e1b9794743fdb9e9224c51abae5dfa61b
Mark-Tevin <tevinsmark@gmail.com>
2023-12-06 08:47:30 -0800

    update

--- 0x03-python-data_structures/2-args.py
+++ 0x03-python-data_structures/2-args.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-
-def no_of_argu(argv):
-
-    num_arg = len(argv) - 1
-    if num_arg == 0:
-        print("{:d} argument.".format(num_arg))
+def print_arg(argv):
+    arg = len(argv) - 1
+    if arg == 0:
+        print("{:d} argument.".format(arg))
         return
     else:
-        if num_arg == 1:
-            print("{:d} argument:".format(num_arg))
+        if arg == 1:
+            print("{:d} argument:".format(arg))
         else:
-            print("{:d} arguments:".format(num_arg))
-        i = 1
-        while i <= num_arg:
-            print("{:d}: {:s}".format(i,argv[i]))
-            i += 1
+            print("{:d} arguments:".format(arg))
+        j = 1
+        while j <= arg:
+            print("{:d}: {:s}".format(j, argv[j]))
+            j += 1
 
 if __name__ == "__main__":
     import sys
-    no_of_argu(sys.argv)
+    print_arg(sys.argv)
