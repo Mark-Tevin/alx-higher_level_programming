619d88faaba021e64023df928eb483c9e3cd3f1c
Mark-Tevin <tevinsmark@gmail.com>
2023-12-07 00:45:01 -0800

    102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    add = __import__('magic_calculation_102', globals(), locals(), ['add'], 0).__dict__['add']
+    sub = __import__('magic_calculation_102', globals(), locals(), ['sub'], 0).__dict__['sub']
+
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        return sub(a, b)
+
+if __name__ == "__main__":
+    import sys
+    magic_calculation(sys.argv)
