622d59b6e590306f9b7451446b07d9c9baf3a963
Mark-Tevin <tevinsmark@gmail.com>
2023-12-09 17:19:21 -0800

    squares of list

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,6 +5,8 @@
     Parameters:
         - matrix (List[List[int]]): 2D list representing the matrix.
     """
+
 def square_matrix_simple(matrix=[]):
     newmatrix = [[i ** 2 for i in row] for row in matrix]
+
     return newmatrix
