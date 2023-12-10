4e464bfd36ac7fadecae3887b8b94fb664d3ce09
Mark-Tevin <tevinsmark@gmail.com>
2023-12-09 17:12:17 -0800

    squares corrected

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""
+    Prints a matrix of integers.
+
+    Parameters:
+        - matrix (List[List[int]]): 2D list representing the matrix.
+    """
 def square_matrix_simple(matrix=[]):
-
-    new_matrix = [[0] * len(matrix[0]) for _ in range(len(matrix))]
-
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            new_matrix[i][j] = matrix[i][j] ** 2
-            return new_matrix
+    newmatrix = [[i ** 2 for i in row] for row in matrix]
+    return newmatrix
