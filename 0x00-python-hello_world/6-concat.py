dbea74a0c92fec4cc37045324d9ae56fce1954c4
Mark-Tevin <tevinsmark@gmail.com>
2024-03-11 19:32:38 -0700

    correct

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Holberton"
 str2 = "School"
-conc = str1 + " " + str2 #this line adds the two strings before they are passed into the f string
+conc = str1 + ' ' + str2 #this line adds the two strings before they are passed into the f string
 print(f"Welcome to {conc}!")
