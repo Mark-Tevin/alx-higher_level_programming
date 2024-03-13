b74535cb9c3d1bc00f80db21aba12c7518b3bf34
Mark-Tevin <tevinsmark@gmail.com>
2024-03-13 06:25:23 -0700

    g

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,8 @@
-#!/usr/bin/python3
-
 # Print all possible combinations of two digits where 01 and 10 are considered the same combination
 for i in range(10):
     for j in range(i, 10):
         if i == 0:
-            print("0{}".format(j), end=',')
+            print("0{}".format(j), end=', ')
         else:
-            print("{}{}".format(i, j), end=',')
+            print("{}{}".format(i, j), end=', ')
 print()
