87c24781554dece292b9915d4f2cf5e8b91f4db3
Mark-Tevin <tevinsmark@gmail.com>
2024-03-13 03:16:17 -0700

    without

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 # ASCII in lowercase with a single print statement, excluding letters 'q' and 'e'
-print(''.join(chr(i) for i in range(97, 123) if chr(i) not in {'q', 'e'}).format(), end='')
+print(''.join(chr(i) for i in range(97, 123) if chr(i) not in {'q', 'e'})
+        .format(), end='')
