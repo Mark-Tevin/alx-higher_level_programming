8e3d93158ae387a806d1c64c7819bdb14d8bed7d
Mark-Tevin <tevinsmark@gmail.com>
2024-03-13 05:26:57 -0700

    quiz5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 # Print numbers from 0 to 99 in ascending order separated by ", " and ending with a newline
-print(', '.join('{}' for _ in range(100)).format(*range(100)))
+print(', '.join('{:02d}' for _ in range(100)).format(*range(100)))
