3479752d3c2f099ab3ce32dc1e35f313f6980cfd
Mark-Tevin <tevinsmark@gmail.com>
2024-03-13 06:54:09 -0700

    try

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,8 @@
 # Print all possible combinations of two digits where 01 and 10 are considered the same combination
 for i in range(10):
     for j in range(i+1, 10):
-        print("{:d}{:d}".format(i, j), end=', ')
         if i == 8 and j == 9:  # Check if it's the combination "89"
-            break  # Stop printing combinations after "89"
+            print("{:d}{:d}".format(i, j), end=' ')  # Print without comma for the last combination
+        else:
+            print("{:d}{:d}".format(i, j), end=', ')
 print()
