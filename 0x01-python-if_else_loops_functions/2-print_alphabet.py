80d70c3fe7892a5a3bafdf2f59497d2598500257
Mark-Tevin <tevinsmark@gmail.com>
2024-03-13 02:04:13 -0700

    process

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 #Print ASCII alphabet in lowercase using a single print statement with string format
-print(''.join(map(chr, range(97, 123))).format(), end='')
-
+print(''.join(chr(i) for i in range(97, 123)).format(), end='')
