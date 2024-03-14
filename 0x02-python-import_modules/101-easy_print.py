ee726fa5217aabdf626b9b802ee1f104a6438cb6
Mark-Tevin <tevinsmark@gmail.com>
2024-03-14 15:13:38 -0700

    try

---
+++ 0x02-python-import_modules/101-easy_print.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+_ = getattr(__import__('builtins'), '__build_class__')  # first line
+exec("_______=__build_class__.__globals__['__builtins__'];_______['p']('#pythoniscool\\n')"  # second line
+
