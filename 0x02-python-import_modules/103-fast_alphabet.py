75692521c2c20c56ea4598e708093e5381cfd0dd
Mark-Tevin <tevinsmark@gmail.com>
2024-03-14 16:32:38 -0700

    try

---
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+import string
+_ = getattr(__import__('sys').modules['builtins'], '__build_class__')
+_._.__globals__['__builtins__']['p'](*map(_._.__globals__['__builtins__']['chr'], range(*map(_._.__globals__['__builtins__']['ord'], ('A', 'Z')+('',))))); _
