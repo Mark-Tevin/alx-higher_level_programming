99c8dd9632d90378209a4ada0f0cafb5b4319666
Mark-Tevin <tevinsmark@gmail.com>
2024-03-17 04:26:05 -0700

    try

---
+++ 0x03-python-data_structures/lists.h
@@ -0,0 +1,18 @@
+# my_header.py
+
+def print_list_integer(my_list[]):
+	'''
+	Prints all the elements in the given list, one integer per line.
+	'''
+	Args:
+	- my_list (list): The list of integers
+	'''
+def element_at(my_list, idx):
+	'''
+	function that retrieves an element from a list like in C
+	'''
+def replace_in_list(my_list, idx, element):
+	'''
+	function that replaces an element of a list at a specific position (like in C).
+	'''
+def print_reversed_list_integer(my_list=[]):
