5ce71ac88ced611e9c22e05488493463ba4bf0ee
Mark-Tevin <tevinsmark@gmail.com>
2024-03-17 11:33:39 -0700

    try

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,10 +2,10 @@
 
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return my_list
-    else:
-        if idx == 3:
-            new_list = [9 if i == 3 else val for i, val in enumerate(my_list)]
-            return new_list
+        return my_list #if the idx is a negative or out of range retun the original list
+    new_list = []
+    for i, elem in enumerate(my_list):
+        if i == 3:
+            new_list.append(9)
         else:
-            return my_list
+            new_list.append(elem)
