f0fda4ef4d88a9858af27950b01e3a7c1550f252
Mark-Tevin <tevinsmark@gmail.com>
2024-03-18 01:14:08 -0700

    try

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,5 +3,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list  # If idx is negative or out of range, return the original list
     else:
-        new_list = [9 if i == 3 else element for i, element in enumerate(my_list)]
+        # create a new list by replacing every element with element, except at index 3
+        new_list = [9 if i == 3 else element for i, _ in enumerate(my_list)]
         return new_list
