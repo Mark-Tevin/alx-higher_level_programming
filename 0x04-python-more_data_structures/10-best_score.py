7a744e8392354d84408543ccbf91d996d0d00b35
Mark-Tevin <tevinsmark@gmail.com>
2024-03-20 15:04:04 -0700

    trying till I die

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+
+    best_key = next(iter(a_dictionary))
+    for key in a_dictionary:
+        if a_dictionary[key] > a_dictionary[best_key]:
+            best_key = key
+    return best_key
