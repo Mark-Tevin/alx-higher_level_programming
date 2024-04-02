d463e1505a391e841f9f0b201bfa22b3af7106bc
Mark-Tevin <tevinsmark@gmail.com>
2024-04-02 07:21:11 -0700

    quiz1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    try:
+        for element in range(x):
+            print(my_list[element], end="")
+            count += 1
+        print()
+    except IndexError:
+        pass
+    print('')
+    return count
