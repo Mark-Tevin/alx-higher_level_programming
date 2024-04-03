4d5c71f489a9425f9af26efb25b63cc8691571fc
Mark-Tevin <tevinsmark@gmail.com>
2024-04-03 06:43:28 -0700

    learn

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Define class square"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """instantiation.
+        Parameters:
+            size: size of the newly created square
+        """
+        if not isinstance(size, int):
+            raise TypeError("Size must be an integer")
+        elif size < 0:
+            raise ValueError("Size must be >= 0")
+        self.__size = size
