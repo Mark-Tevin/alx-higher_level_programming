3bf60acec33bb20b008b378c0f1c42fa7e8859cd
Mark-Tevin <tevinsmark@gmail.com>
2024-04-03 03:53:45 -0700

    learn

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,7 +4,14 @@ class Square:
     """
     Represents a square.
 
+    The Square class is designed to represent a square shape. It does not contain any specific attributes or methods.
+    Instances of this class can be used to represent squares in various contexts.
+
     Attributes:
         None
+
+    Purpose:
+        The purpose of the Square class is to provide a basic representation of a square shape in Python programs.
+        It serves as a foundation for more complex square-related functionality that may be built upon it.
     """
     pass
