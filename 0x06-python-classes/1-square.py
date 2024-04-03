e72942729bfb6103bb14230451a7b84f6382dae0
Mark-Tevin <tevinsmark@gmail.com>
2024-04-03 04:30:13 -0700

    learn

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""defined class."""
+
+
+class square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """Initialization
+
+        parameter:
+
+        size: size of new square.
+        """
+        self.__size = size
