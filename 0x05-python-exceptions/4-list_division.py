648013b4a0b62734e9a1c3b9a64cb84d40555b5c
Mark-Tevin <tevinsmark@gmail.com>
2024-04-02 19:45:50 -0700

    changed

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -11,22 +11,19 @@ def list_division(my_list_1, my_list_2, list_length):
             result.append(division_result)
         except ZeroDivisionError:
             errors.append("division by 0")
-            result.append(None)
+            result.append(0)  # Set result to 0 when division by 0 occurs
         except IndexError:
             errors.append("out of range")
-            result.append(None)
+            result.append(0)  # Set result to 0 when index is out of range
         except TypeError:
             errors.append("wrong type")
-            result.append(None)
+            result.append(0)  # Set result to 0 when there is a type error
         except Exception as e:
             errors.append("an error occurred: {}".format(e))
-            result.append(None)
+            result.append(0)  # Set result to 0 when other exceptions occur
 
     if errors:
         for error in errors:
             print(error)
-
-    # Replace None values with 0
-    result = [0 if value is None else value for value in result]
 
     return result
