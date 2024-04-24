96f3e502d069ed1892359a62b9abe627982f45c4
Mark-Tevin <tevinsmark@gmail.com>
2024-04-24 05:27:40 -0700

    returns list of attributes and method

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    This file returns lists of variable available and methods
+    of and object
+"""
+
+def lookup(obj):
+    """This function looks out for all attributes and methods available"""
+    return dir(obj)
