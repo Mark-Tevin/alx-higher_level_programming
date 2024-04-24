d7b5fda56c769e9eaca15aa034a910a5685729e8
Mark-Tevin <tevinsmark@gmail.com>
2024-04-24 11:55:57 -0700

    Only sub class

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-"""This module checks if  is an instance of a class
-that inherited (directly or indirectly)
-from the specified class.
+"""checks if object is an instance of a class that
+inherited from the specified class or not
 """
 
 
 def inherits_from(obj, a_class):
-    """Function checks if object is an instance of a class
-    :param obj: The object to check.
-    :param a_class: The class to check against.
-    :return: True if is an instance of subclass,
-    else False if otherwise.
+    """Returns true if object is an instance of a class that inherited
+    from the specified class; otherwise False
     """
     return (issubclass(type(obj), a_class) and type(obj) != a_class)
