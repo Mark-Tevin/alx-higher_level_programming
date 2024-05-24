a5bda4f8fa6ffe161f72ac65dc460dece34418cb
Mark-Tevin <tevinsmark@gmail.com>
2024-05-24 16:39:33 -0700

    Only comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,11 +1,11 @@
--- lists all Comedy shows in the database hbtn_0d_tvshows
--- The tv_genres table contains only one record where name = Comedy
+-- lists all Comedy shows in the database hbtn_0d_tvshows.
+-- The tv_genres table contains only one record where name = Comedy (but the id can be different)
 -- Each record should display: tv_shows.title
 -- Results must be sorted in ascending order by the show title
--- use only one SELECT statement
+-- You can use only one SELECT statement
 -- The database name will be passed as an argument of the mysql command
 
-SELCT tv_shows.title
+SELECT tv_shows.title
 FROM tv_shows
 INNER JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
