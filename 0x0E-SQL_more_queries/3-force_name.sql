1a0ec6ce34f124afcc976a58d0315dfe223231f0
Mark-Tevin <tevinsmark@gmail.com>
2024-05-24 14:31:25 -0700

    creates table

---
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -0,0 +1,4 @@
+-- creates the table force_name
+-- The database name will be passed as an argument
+-- If the table force_name already exists, your script should not fail
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
