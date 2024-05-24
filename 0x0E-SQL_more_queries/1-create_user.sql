dfdf52158470efb61b5a47ce238e2807f975b84a
Mark-Tevin <tevinsmark@gmail.com>
2024-05-24 14:02:05 -0700

    Grant all privileges to a user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,7 @@
 -- creates user user_0d_1 with priviledges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
-IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
   ON *.*
   TO 'user_0d_1'@'localhost'
