1b66ecfb2314a7feedf8c35eb4fa34426dbcc828
Mark-Tevin <tevinsmark@gmail.com>
2024-05-24 14:54:55 -0700

    States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,13 +1,6 @@
 -- creates the database hbtn_0d_usa and the table states
 -- If the database hbtn_0d_usa already exists, script should not fail
 -- If the table states already exists, script should not fail
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
-
--- Switch to the database btn_0d_usa
-USE btn_0d_usa;
-
--- Create the table states if it does not exist
-CREATE TABLE IF NOT EXISTS states (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(256) NOT NULL
-);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(256) NOT NULL);
