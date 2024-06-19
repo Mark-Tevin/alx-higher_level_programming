30e4772b1a61e779d0dc23cf553a1fd4f3896071
Mark-Tevin <tevinsmark@gmail.com>
2024-06-19 09:58:30 -0700

    prints the first argument passed to it

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/*
+ * prints the first argument passed to it
+*/
+const args = process.argv.slice(2);
+
+if (args[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(args[0]);
+}
