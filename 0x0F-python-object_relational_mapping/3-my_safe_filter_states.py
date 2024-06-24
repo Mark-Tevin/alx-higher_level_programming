12c313cc6fc4711f885bc30955fe7ac27e9da49e
Mark-Tevin <tevinsmark@gmail.com>
2024-06-23 19:00:21 -0700

    SQL Injection...

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,35 @@
 #!/usr/bin/python3
+
 """
-Script that takes in args and displays all values in the states
-table to hbtn_0e_0_usa where name matches the argument, safe from MySQL
-injections
+    A script that lists all states from the database hbtn_0e_0_usa
+    starting with capital letter N
+    Username, password and database names are given as user args
 """
+
+
+import sys
 import MySQLdb
-from sys import argv
 
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = %s ORDER BY id ASC"
-                (argv[4],))
-    rows = cur.fetchall()
-    for row in rows:
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3],
+                         host='localhost',
+                         port=3306)
+
+    cursor = db.cursor()
+
+    sql = """SELECT * FROM states
+          WHERE name = %s
+          ORDER BY id ASC"""
+
+    cursor.execute(sql, (sys.argv[4],))
+
+    data = cursor.fetchall()
+
+    for row in data:
         print(row)
-    cur.close()
+
+    cursor.close()
     db.close()
