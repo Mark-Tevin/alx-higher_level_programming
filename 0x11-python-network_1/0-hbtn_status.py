f602218bc533b3a0bd5a56c56e3ed4553d6de419
Mark-Tevin <tevinsmark@gmail.com>
2024-07-07 10:51:34 -0700

    What's my status? #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
-
+import urllib.error
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf-8 content: {}".format(body.decode("utf-8")))
+    url = "https://alx-intranet.hbtn.io/status"
+    
+    try:
+        request = urllib.request.Request(url)
+        with urllib.request.urlopen(request) as response:
+            body = response.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(body)))
+            print("\t- content: {}".format(body))
+            print("\t- utf-8 content: {}".format(body.decode("utf-8")))
+    except urllib.error.URLError as e:
+        print(f"Failed to fetch {url}: {e.reason}")
