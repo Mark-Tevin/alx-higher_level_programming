450b7e040a9719cc63383793a767a413e2fe4c31
Mark-Tevin <tevinsmark@gmail.com>
2024-07-31 19:36:59 -0700

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,23 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 request(process.argv[2], function (error, response, body) {
-  if (!error) {
-    const results = JSON.parse(body).results;
+  if (error) {
+    console.error('Request failed:', error);
+    return;
+  }
+
+  try {
+    const data = JSON.parse(body);
+    const results = data.results || [];
+
     console.log(results.reduce((count, movie) => {
-      return movie.characters.find((character) => character.endsWith('/18/'))
+      return movie.characters && movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
     }, 0));
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
   }
 });
