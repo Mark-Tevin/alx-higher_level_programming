23626949ea3dbaccb21b1a43dc5f1dcad0fcb450
Mark-Tevin <tevinsmark@gmail.com>
2024-08-14 07:27:14 -0700

    Add jQuery script to update header text on click of update_header div

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,6 +1,6 @@
-// script that updates the test of <header> element to New Header!!
-// when the user clicks on DIV#update_header
-const $ = window.$;
-$('#update_header').bind('click', function () {
-  $('header').replacewith('New Header!!!');
+//  updates the text of the <header> element to New Header!!!
+$(document).ready(function () {
+  $('#update_header').click(function () {
+    $('header').text('New Header!!!');
+  });
 });
