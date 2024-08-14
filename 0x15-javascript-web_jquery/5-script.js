977bf46c8ab4dcd8a58ee3371c0d4d56085d3602
Mark-Tevin <tevinsmark@gmail.com>
2024-08-14 06:28:35 -0700

    Add jQuery script to dynamically append new list items on click of add_item div

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,5 +1,5 @@
 // script that adds a <li> element to a list when the user clicks
-// the tag DIV#add_item
+// DIV#add_item tag
 const $ = window.$;
 $('#add_item').bind('click', function () {
   $('UL.my_list').append('<li>Item</li>');
