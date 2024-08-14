8df413ff4ac4f12c5bcc9e4e01beb75129a05e1a
Mark-Tevin <tevinsmark@gmail.com>
2024-08-14 05:26:05 -0700

    Add jQuery script to change header color on click event

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,6 +1,7 @@
 // script that updates the next text color of the <header> element
 // to red (#FF0000) when the user clicks on the tag DIV#red_header:
-const $ = window.$;
-$('#red_header').bind('click', function () {
-  $('header').css({ color: '#FF0000' });
+$(document).ready(function () {
+  $('#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
 });
